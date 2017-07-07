bda8a5d647780d6d2bbb38ec6ac4ffabc55782a6
Gabriel Cano <gcano@m-C1ML86JLDTY4.s08771.gec.br.wal-mart.com>
2017-07-07 09:27:53 -0300

    First Files

---
+++ Programa.java
@@ -0,0 +1,36 @@
+class Programa {
+
+	public static void main (String [] args){
+		int idadeJoao = 18;
+		int idadeMaria = 21;
+
+		int somaDasIdades = idadeJoao + idadeMaria;
+		System.out.println(somaDasIdades);
+
+
+
+		int numero = 10;
+		System.out.println(numero);
+
+		double pi = 3.1415;
+		double dobroDoPi = pi * 2;
+		
+		System.out.println (dobroDoPi);
+
+
+
+		boolean amigo = true;
+		boolean inimigo = !amigo;
+
+		boolean maiorDeIdade = idadeJoao <= 18;
+		
+		char letra = 'M';
+
+		String nomeCompleto = "João da Silva" ;
+
+		System.out.println (nomeCompleto);
+
+
+
+	}
+}
---
+++ TerceiroPrograma.java
@@ -0,0 +1,20 @@
+class TerceiroPrograma {
+
+	public static void main (String [] args){
+
+	int idadeVisitante = 10;
+	boolean amigoDoDono = true;
+
+
+	if (idadeVisitante>60){
+		System.out.println ("Aproveite a Melhor Idade!");
+	}
+	else if (idadeVisitante>=18 && amigoDoDono){
+		System.out.println ("Entre na festa!");
+	}
+		else{
+			System.out.println ("Volte para Casa!");
+		}
+	}
+
+	}
